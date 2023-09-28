f56888c1198043481daa57ff168feb5f7be9a5b0
Abasay <abdulsalamasheem@gmail.com>
2023-09-28 03:53:22 +0000

    corrections

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -7,15 +7,15 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       const resArr = [];
 
       values.forEach((value) => {
-      if (value.status === 'fulfilled') {
-	resArr.push(value);
-      } else {
-	resArr.push({
-          status: value.status,
-          value: `Error: ${value.reason.message}`,
-	});
-      }
-    });
-    return resArr;
+        if (value.status === 'fulfilled') {
+          resArr.push(value);
+        } else {
+          resArr.push({
+            status: value.status,
+            value: `Error: ${value.reason.message}`,
+          });
+        }
+      });
+      return resArr;
     });
 }
