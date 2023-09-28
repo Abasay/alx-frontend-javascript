38eecd7c839852f1c9d7c1b96f5a4a709c396dea
Abasay <abdulsalamasheem@gmail.com>
2023-09-28 03:33:37 +0000

    corrections

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,13 @@
 export default function handleResponseFromAPI(promise) {
-    return promise
+  return promise
 	.then(() => {
-	    return {
-                status : 200,
-                body : 'success'
-            };
-        })
-        .catch((err) => new Error())
-        .then(() => {
-            console.log('Got a response from the API');
+	    {
+		status: 200,
+		body: 'success',
+	    }
 	})
+    .catch((err) => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,20 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((res) => res);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => {
+      const resArr = [];
+
+      values.forEach((value) => {
+      if (value.status === 'fulfilled') {
+	resArr.push(value);
+      } else {
+	resArr.push({
+          status: value.status,
+          value: `Error: ${value.reason.message}`,
+	});
+      }
+    });
+    return resArr;
+    });
 }
