f743c930452693263dfd8d7aa7773d78e9ca1d80
Abasay <abdulsalamasheem@gmail.com>
2023-09-29 00:30:27 +0000

    4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // get amount
+  get amount() {
+    return this._amount;
+  }
+
+  // set amount
+  set amount(val) {
+    this._amount = val;
+  }
+
+  // get currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(val) {
+    this._currency = val;
+  }
+
+  // display full price method
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} ${this._currency.code}`;
+  }
+}
