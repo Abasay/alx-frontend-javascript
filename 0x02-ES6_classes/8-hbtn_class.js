4a1a9d8fc100849eb9b0776ffcbb44ca6a87fba2
Abasay <abdulsalamasheem@gmail.com>
2023-09-29 04:47:18 +0000

    file

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -2,12 +2,13 @@ export default class HolbertonClass {
   constructor(size, location) {
     this._size = size;
     this._location = location;
+  }
 
-    if(typeof(this.prototype) === 'NaN') {
-      return size;
-    }
-    if(typeof(this) === 'string') {
-      return location;
-    }
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
   }
 }
