db70bfa9165d0ff440d24433f7b4f41099004ecd
Abasay <abdulsalamasheem@gmail.com>
2023-09-29 00:05:55 +0000

    classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,61 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+    if (typeof (length) !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      this._students = students;
+    }
+  }
+
+  // get name
+  get name() {
+    return this._name;
+  }
+
+  // set name
+  set name(value) {
+    if (typeof (value) !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = value;
+    }
+  }
+
+  // get length
+  get length() {
+    return this._length;
+  }
+
+  // set length
+  set length(value) {
+    if (typeof (value) !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = value;
+    }
+  }
+
+  // get Students
+  get students() {
+    return this._students;
+  }
+
+  // set students
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw new TypeError('Students must be an array');
+    } else {
+      this._students = value;
+    }
+  }
+}
