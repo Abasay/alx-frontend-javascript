8377e56f091a3baf4ff390f553a66bfea8cfb791
Abasay <abdulsalamasheem@gmail.com>
2023-10-04 00:42:39 +0000

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,26 +1,9 @@
-export default function cleanSet(setArg, startString) {
-  const resArr = [];
-  const deleteCount = startString.length;
-  let state = false;
-  for (const elem of setArg) {
-    for (let i = 0; i < startString.length; i += 1) {
-      if (startString[i] !== elem[i]) {
-        state = false;
-        break;
-      } else {
-        state = true;
-      }
-    }
-    if (state) {
-      resArr.push(elem);
-    }
+export default function cleanSet(set, startString) {
+  if (startString === undefined || startString.length === 0) {
+    return '';
   }
-  const anoArr = [];
-  for (const elem of resArr) {
-    const elemArr = Array.from(elem);
-    const spliced = elemArr.splice(deleteCount, elem.length);
-    const joined = spliced.join('');
-    anoArr.push(joined);
-  }
-  return anoArr.join('-');
+  return [...set]
+    .filter((parametro) => (parametro !== undefined ? parametro.startsWith(startString) : ''))
+    .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''))
+    .join('-');
 }
